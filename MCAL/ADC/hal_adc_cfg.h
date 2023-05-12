8c0219252f7f9f0a7f1bfa218992556d38bb0219
Ahmed Atef Ahmed <116688909+ahmedatefmetawaa@users.noreply.github.com>
2023-05-12 18:59:01 +0300

    Add files via upload

---
+++ MCAL/ADC/hal_adc_cfg.h
@@ -0,0 +1,24 @@
+
+/* 
+ * File:   
+ * Author: 
+ * Comments:
+ * Revision history: 
+ */
+
+  
+#ifndef HAL_ADC_CFG_H
+#define	HAL_ADC_CFG_H
+/*section : includes*/
+
+/*section : macro declerations*/
+
+/*section : macro functions declerations */
+
+/*section : data types declerations*/
+
+/*section : functions declerations*/
+
+
+#endif	/* HAL_ADC_CFG_H */
+
